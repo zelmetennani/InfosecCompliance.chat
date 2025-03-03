c6497ebc71199eac7f033517809f5ecd8c36c7c9
Zack <zacharyelmetennani@gmail.com>
2025-03-03 16:08:07 +0700

    su fix

--- config.js
+++ config.js
@@ -70,16 +70,4 @@ if (window.firebaseConfigInitialized) {
         console.log("Dispatching firebaseConfigReady event");
         document.dispatchEvent(new Event('firebaseConfigReady'));
     }
-}
-
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig); 
+} 
