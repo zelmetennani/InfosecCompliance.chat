6a02ee10e1c88e57a439a10ec2ab25924bcc7649
Zack <zacharyelmetennani@gmail.com>
2025-03-03 16:10:39 +0700

    sign up with debugging fix

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ if (window.firebaseConfigInitialized) {
     
     // For production environment - this will be replaced by GitHub Actions
     window.firebaseConfig = {
-        apiKey: "FIREBASE_API_KEY",
-        authDomain: "FIREBASE_AUTH_DOMAIN",
-        projectId: "FIREBASE_PROJECT_ID",
-        storageBucket: "FIREBASE_STORAGE_BUCKET",
-        messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
-        appId: "FIREBASE_APP_ID",
-        measurementId: "FIREBASE_MEASUREMENT_ID"
+        apiKey: "##FIREBASE_API_KEY##",
+        authDomain: "##FIREBASE_AUTH_DOMAIN##",
+        projectId: "##FIREBASE_PROJECT_ID##",
+        storageBucket: "##FIREBASE_STORAGE_BUCKET##",
+        messagingSenderId: "##FIREBASE_MESSAGING_SENDER_ID##",
+        appId: "##FIREBASE_APP_ID##",
+        measurementId: "##FIREBASE_MEASUREMENT_ID##"
     };
     
     console.log("Firebase config set:", window.firebaseConfig);
